69283304824ddbd6dae439ca37995eb58b9597ea
Firoz Ansari <firozansar@gmail.com>
2022-04-14 07:58:26 +0100

    Refactoring

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-# CSV reader
+# CSV Data Sorter
 
-This is a program that reads data from a given JSON file, sorts the data according to the user's selection, and outputs it in the CSV file format.
-
-The character code is UTF-8.
-The language used is Kotlin.
-No external library is used.
-
-I created and added a so-called fatjar file that includes external libraries.
-You can check the operation by putting the CsvDataSortInKotlin-all.jar file and the UTF8Test.csv file in the same folder.
+This is a program that reads data from a given CSV file, sorts the data according to the user's selection, and outputs it in the CSV file format.
+Written in [Kotlin](https://kotlinlang.org/) and the only library used is [kotlin-csv](https://github.com/doyaaaaaken/kotlin-csv/)
--- src/main/kotlin/info/firozansari/FileOperations.kt
+++ src/main/kotlin/info/firozansari/FileOperations.kt
@@ -1,34 +1,45 @@
 package info.firozansari
 
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import java.io.File
 import java.util.StringTokenizer
 
 import kotlin.system.exitProcess
 
 fun csvFileOperation(sortNumber: Int, sortOrder: Int){
-	val csvFile = getResourceText("UTF8Test.csv")
 	val temporaryStudentList = mutableListOf<Student>()
-
-	csvFile.forEachLine {
-		if (it.isNotBlank()) {
-			val tokenizer = StringTokenizer(it)
-			val studentNumber = (tokenizer.nextToken(","))
-			val studentName = (tokenizer.nextToken(","))
-			val studentCourse = (tokenizer.nextToken(","))
-			val studentAge = (tokenizer.nextToken(","))
-			val studentScore = (tokenizer.nextToken(","))
-
-			val hoge = Student(
-					studentNumber,
-					studentName,
-					studentCourse,
-					stringToInt(studentAge),
-					stringToInt(studentScore)
-			)
-			temporaryStudentList.add(hoge)
-		}
+	val csvFile = getResourceText("UTF8Test.csv")
+	val rows: List<List<String>> = csvReader().readAll(csvFile)
+	rows.forEach { row: List<String> ->
+		println(row)
 	}
-	sortList(sortNumber, sortOrder, temporaryStudentList)
+//	csvReader().open("UTF8Test.csv") {
+//		readAllAsSequence().forEach { row: List<String> ->
+//			//Do something
+//			println(row) //[a, b, c]
+//		}
+//	}
+
+//	csvFile.forEachLine {
+//		if (it.isNotBlank()) {
+//			val tokenizer = StringTokenizer(it)
+//			val studentNumber = (tokenizer.nextToken(","))
+//			val studentName = (tokenizer.nextToken(","))
+//			val studentCourse = (tokenizer.nextToken(","))
+//			val studentAge = (tokenizer.nextToken(","))
+//			val studentScore = (tokenizer.nextToken(","))
+//
+//			val hoge = Student(
+//					studentNumber,
+//					studentName,
+//					studentCourse,
+//					stringToInt(studentAge),
+//					stringToInt(studentScore)
+//			)
+//			temporaryStudentList.add(hoge)
+//		}
+//	}
+//	sortList(sortNumber, sortOrder, temporaryStudentList)
 }
 
 fun stringToInt(stringLikeInt : String):Int{
